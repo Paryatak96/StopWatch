98e5db175e980ca38c8de6dfb0f71b7d1cc9a5dc
Sebastian <Sebastian.Liszewski@outlook.com>
2022-07-30 21:13:42 +0200

    StopWatch - first adding.

---
+++ Zadanie 1 - Stoper/Zadanie 1 - Stoper/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Zadanie_1___Stoper
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var przelacznik = true;
+            do
+            {
+                System.Console.Clear();
+                Console.WriteLine("Co chcesz zrobić?");
+                Console.WriteLine("1 - Stoper Start");
+                Console.WriteLine("2 - Stoper Stop");
+                var przycisk = byte.Parse(Console.ReadLine());
+                switch (przycisk)
+                {
+                    case 1:
+                        {
+                            if (przelacznik == true)
+                            {
+                                Stoper.Start();
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("NIE MOŻNA UŻYĆ 'Stoper Start' PRZED ZATRZYMANIEM!");
+                            }
+                            przelacznik = false;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Stoper.Stop();
+                            przelacznik = true;
+                            Console.WriteLine();
+                            Console.ReadLine();
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Zadanie 1 - Stoper/Zadanie 1 - Stoper/Stoper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Zadanie_1___Stoper
+{
+    class Stoper
+    {
+        private readonly static Stopwatch stopwatch = new Stopwatch();
+
+        public static void Start ()
+        {
+            stopwatch.Start();   
+        }
+        public static void Stop()
+        {
+            stopwatch.Stop();
+            TimeSpan time = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", time.Hours, time.Minutes, time.Seconds);
+            Console.WriteLine("Zmierzony czas to: " + elapsedTime);
+            Console.WriteLine("Naciśnij 'ENTER' aby powrócić!");
+            Console.ReadLine();
+            stopwatch.Reset();
+        }
+    }
+}
